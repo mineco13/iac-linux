6e07a2ed6c55783f31fee2d5bd909f5372056e08
root <root@ip-10-0-153-140.ec2.internal>
2021-04-08 16:35:47 +0000

    add sh to install docker

---
+++ docker_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+echo Y|apt install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+"deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+echo Y|apt install docker-ce docker-ce-cli containerd.io
